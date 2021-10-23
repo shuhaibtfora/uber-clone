ac8fad16e1bf722c4f8225ad15b57e54f696bef1
shuhaibtfora <shuhaib.tfora@gmail.com>
2021-10-23 14:27:42 +0530

    first commit

---
+++ jqury.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+//     $("#btt").click(function(){
+//         alert("haii")
+//     })
+// })
+
+$(document).ready(function(){
+    $("#btt").click(function(){
+        $("#popup").show()
+        $("#transp").show()
+        
+        
+    })
+
+    $("#close").click(function(){
+        $("#popup").hide()
+        $("#transp").hide()
+        
+    })
+
+})
